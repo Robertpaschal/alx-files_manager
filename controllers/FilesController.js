986706eb1c25e28393f08e68f0cc94ce52d37cc1
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-08-05 18:20:34 +0100

    first file

---
+++ controllers/FilesController.js
@@ -0,0 +1,76 @@
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const path = require('path');
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+const FOLDER_PATH = process.env.FOLDER_PATH || 'tmp/files_manager';
+
+if (!fs.existsSync(FOLDER_PATH)) {
+  fs.mkdirSync(FOLDER_PATH, { recursive: true });
+}
+
+const FilesController = {
+  async postUpload(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const {
+      name, type, parentId, isPublic = false, data,
+    } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Missing name' });
+    }
+
+    if (!type || !['folder', 'file', 'image'].includes(type)) {
+      return res.status(400).json({ error: 'Missing or invalid type' });
+    }
+
+    if (type !== 'folder' && !data) {
+      return res.status(400).json({ error: 'Missing data' });
+    }
+
+    if (parentId) {
+      const parentFile = await dbClient.db.collection('files').findOne({ _id: parentId });
+      if (!parentFile) {
+        return res.status(400).json({ error: 'Parent not found' });
+      }
+
+      if (parentFile.type !== 'folder') {
+        return res.status(400).json({ error: 'Parent is not a folder' });
+      }
+    }
+
+    const file = {
+      userId,
+      name,
+      type,
+      isPublic,
+      parentId: parentId || 0,
+      localPath: '',
+    };
+
+    if (type === 'folder') {
+      const result = await dbClient.db.collection('files').insertOne(file);
+      return res.status(201).json(result.ops[0]);
+    }
+    const fileId = uuidv4();
+    const filePath = path.join(FOLDER_PATH, `${fileId}_${name}`);
+
+    fs.writeFileSync(filePath, Buffer.from(data, 'base64'));
+
+    file.localPath = filePath;
+    const result = await dbClient.db.collection('files').insertOne(file);
+    return res.status(201).json(result.ops[0]);
+  },
+};
+
+module.exports = FilesController;
